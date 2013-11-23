2f1bc5874a2b4665b60a73b9c6dc1a8733e5bc3b
John Woods <jwoods@vt.edu>
2013-11-22 18:31:20 -0600

    Updated Mac OS X install scripts for Mavericks

--- scripts/mac-brew-gcc.sh
+++ scripts/mac-brew-gcc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-VERSION="4.7.2" # Script should also work with GCC 4.7.2.
+# Script will not work for GCC 4.8 or 4.9. For those, please see
+# mac-mavericks-brew-gcc.sh
+
+VERSION="4.7.2" # Script should also work with GCC 4.7.1.
 PREFIX="/usr/gcc-${VERSION}"
 LANGUAGES="c,c++,fortran"
 MAKE="make -j 4"
@@ -10,31 +13,6 @@ brew-path() { brew info $1 | head -n3 | tail -n1 | cut -d' ' -f1; }
 # Prerequisites
 
 brew install gmp mpfr libmpc
-
-# Try using the following for GCC 4.9:
-#
-#   brew install gmp4 mpfr2 libmpc08 isl011 cloog018
-#
-#
-
-# For Mavericks, you may also need to install isl and cloog. I couldn't get this script to work
-# in Mavericks (at least for GCC 4.8.2), but was able to do:
-#
-#   brew install gcc49 --enable-fortran
-#
-# Source for this is: http://stackoverflow.com/questions/19535422/os-x-10-9-gcc-links-to-clang
-#
-# Note that NMatrix doesn't currently require Fortran, but dependent libraries may eventually.
-# If you wanted to install isl and cloog, you'd need to do the following:
-#
-#   brew install isl
-#
-# if you uncomment the above line, also add --with-isl=$(brew --prefix isl)
-# to the configure command below. cloog is:
-#
-#   brew install cloog  # this needs --with-cloog=$(brew --prefix cloog)
-#
-
 
 # Next, download & install the latest GCC:
 
