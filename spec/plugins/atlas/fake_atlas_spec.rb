507951223890302048d212a0b05e52d1eee6da8f
Will Levine <wlevine@gmail.com>
2015-05-28 18:20:27 -0400

    Add clone of clapack_getri in nmatrix_atlas

--- spec/plugins/atlas/fake_atlas_spec.rb
+++ spec/plugins/atlas/fake_atlas_spec.rb
@@ -10,4 +10,22 @@ describe "nmatrix-atlas" do
     n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)
     expect(n.test_c_ext_return_2).to eq(2)
   end
+
+  [:rational32, :rational64, :rational128, :float32, :float64, :complex64, :complex128].each do |dtype|
+    context dtype do
+      it "exposes clapack_getri_test" do
+        a = NMatrix.new(:dense, 3, [1,0,4,1,1,6,-3,0,-10], dtype)
+        ipiv = NMatrix::LAPACK::clapack_getrf(:row, 3, 3, a, 3) # get pivot from getrf, use for getri
+
+        begin
+          NMatrix::LAPACK::clapack_getri_test(:row, 3, a, 3, ipiv)
+
+          b = NMatrix.new(:dense, 3, [-5,0,-2,-4,1,-1,1.5,0,0.5], dtype)
+          expect(a).to eq(b)
+        rescue NotImplementedError => e
+          pending e.to_s
+        end
+      end
+    end
+  end
 end
