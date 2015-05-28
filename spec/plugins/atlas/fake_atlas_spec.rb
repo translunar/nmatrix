38702dadf5ad5edf086733375cf378b07782e2ea
Will Levine <wlevine@gmail.com>
2015-05-28 12:58:27 -0400

    Fix fake_atlas_spec

--- spec/plugins/atlas/fake_atlas_spec.rb
+++ spec/plugins/atlas/fake_atlas_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
+require "./lib/nmatrix/atlas"
 
 describe "nmatrix-atlas" do
   it "returns 3" do
     n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)
     expect(n.test_return_3).to eq(3)
+  end
+  it "returns 2" do
+    n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)
+    expect(n.test_c_ext_return_2).to eq(2)
   end
 end
