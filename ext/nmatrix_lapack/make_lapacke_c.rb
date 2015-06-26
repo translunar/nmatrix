34e125e80fd32df92b388c1d0dcac1a3bd66b207
Will Levine <wlevine@gmail.com>
2015-06-26 14:17:06 -0400

    comment fixjk:wq

--- ext/nmatrix_lapack/make_lapacke_c.rb
+++ ext/nmatrix_lapack/make_lapacke_c.rb
@@ -1,4 +1,4 @@
-#We want this to be a C++ file since our complex types require C++. Do they actually?
+#We want this to be a C++ file since our complex types require C++.
 
 File.open("lapacke.cpp","w") do |file|
   Dir["lapacke/**/*.c"].each do |file2|
