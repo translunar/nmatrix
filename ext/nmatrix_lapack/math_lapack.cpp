5c6ad0ca34897b9aea4bfc531c3d91140c79ae83
Will Levine <wlevine@gmail.com>
2015-06-30 11:10:17 -0400

    Move getri functions to header

--- ext/nmatrix_lapack/math_lapack.cpp
+++ ext/nmatrix_lapack/math_lapack.cpp
@@ -5,48 +5,7 @@
 #include "math/cblas_enums.h"
 #include "math/util.h"
 
-namespace nm { namespace math { namespace lapack {
-
-template <typename DType>
-inline int getri(const enum CBLAS_ORDER order, const int n, DType* a, const int lda, const int* ipiv) {
-  rb_raise(rb_eNotImpError, "getri not yet implemented for non-BLAS dtypes");
-  return 0;
-}
-
-template <>
-inline int getri(const enum CBLAS_ORDER order, const int n, float* a, const int lda, const int* ipiv) {
-  return LAPACKE_sgetri(order, n, a, lda, ipiv);
-}
-
-template <>
-inline int getri(const enum CBLAS_ORDER order, const int n, double* a, const int lda, const int* ipiv) {
-  return LAPACKE_dgetri(order, n, a, lda, ipiv);
-}
-
-template <>
-inline int getri(const enum CBLAS_ORDER order, const int n, Complex64* a, const int lda, const int* ipiv) {
-  return LAPACKE_cgetri(order, n, a, lda, ipiv);
-}
-
-template <>
-inline int getri(const enum CBLAS_ORDER order, const int n, Complex128* a, const int lda, const int* ipiv) {
-  return LAPACKE_zgetri(order, n, a, lda, ipiv);
-}
-
-/*
- * Function signature conversion for calling LAPACK's getri functions as directly as possible.
- *
- * For documentation: http://www.netlib.org/lapack/double/dgetri.f
- *
- * This function should normally go in math.cpp, but we need it to be available to nmatrix.cpp.
- */
-template <typename DType>
-inline int clapack_getri(const enum CBLAS_ORDER order, const int n, void* a, const int lda, const int* ipiv) {
-  return getri<DType>(order, n, static_cast<DType*>(a), lda, ipiv);
-}
-
-
-} } } // end nm::math::lapack
+#include "math_lapack/getri_lapack.h"
 
 /*
  * Forward Declarations
