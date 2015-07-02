481797f1a4a88ff330c775a7d9a0218e34280992
Will Levine <wlevine@gmail.com>
2015-07-02 14:53:26 -0400

    Re-add lapack.rb, which shouldn't have been removed

---
+++ lib/nmatrix/lapack.rb
@@ -0,0 +1,6 @@
+require 'nmatrix' #need to have nmatrix required first or else bad things will happen
+require_relative 'lapack_ext_common'
+
+NMatrix.register_lapack_extension("nmatrix-lapack")
+
+require "nmatrix_lapack.so"
