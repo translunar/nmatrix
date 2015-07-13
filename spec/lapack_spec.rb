0189472aa416c623a4882f7242d33735aa258cb8
Will Levine <wlevine@gmail.com>
2015-07-13 11:10:52 -0400

    Add posv spec

--- spec/lapack_spec.rb
+++ spec/lapack_spec.rb
@@ -173,6 +173,29 @@ describe "NMatrix::LAPACK functions with internal implementations" do
         expect(b).to be_within(err).of(x)
       end
 
+      #posv is like potrf+potrs
+      it "exposes clapack_posv" do
+        pending "posv requires clapack" unless NMatrix.has_clapack?
+
+        a = NMatrix.new(3, [4, 0,-1,
+                            0, 2, 1,
+                            0, 0, 1], dtype: dtype)
+        b = NMatrix.new([3,1], [3,0,2], dtype: dtype)
+
+        NMatrix::LAPACK::clapack_posv(:row, :upper, 3, 1, a, 3, b, 3)
+
+        x = NMatrix.new([3,1], [3.5, -5.5, 11], dtype: dtype)
+
+        err = case dtype
+                when :float32, :complex64
+                  1e-5
+                when :float64, :complex128
+                  1e-14
+              end
+
+        expect(b).to be_within(err).of(x)
+      end
+
       # Together, these calls are basically xGESV from LAPACK: http://www.netlib.org/lapack/double/dgesv.f
       # Spec OK
       # Doesn't work is b isn't a vector. Should add a spec for this when it's fixed.
