455dccb12ff7f24d1b4f2d8665c614bfe3de929b
Will Levine <wlevine@gmail.com>
2015-08-19 16:03:36 -0400

    Re-enable optimization

--- ext/nmatrix/extconf.rb
+++ ext/nmatrix/extconf.rb
@@ -158,9 +158,9 @@ end
 #$libs += " -lprofiler "
 
 # For release, these next two should both be changed to -O3.
-$CFLAGS += " -O0 -g "
+$CFLAGS += " -O3 "
 #$CFLAGS += " -static -O0 -g "
-$CPPFLAGS += " -O0 -g -std=#{$CPP_STANDARD} " #-fmax-errors=10 -save-temps
+$CPPFLAGS += " -O3 -std=#{$CPP_STANDARD} " #-fmax-errors=10 -save-temps
 #$CPPFLAGS += " -static -O0 -g -std=#{$CPP_STANDARD} "
 
 CONFIG['warnflags'].gsub!('-Wshorten-64-to-32', '') # doesn't work except in Mac-patched gcc (4.2)
