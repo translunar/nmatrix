ad660c96faa96de8878eab830759713eeef9f90e
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-11-29 01:41:20 +0530

    Added functions to extconf for proper linking with FFTW3

---
+++ ext/nmatrix_fftw/nmatrix_fftw.cpp
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////////////
+// = NMatrix
+//
+// A linear algebra library for scientific computation in Ruby.
+// NMatrix is part of SciRuby.
+//
+// NMatrix was originally inspired by and derived from NArray, by
+// Masahiro Tanaka: http://narray.rubyforge.org
+//
+// == Copyright Information
+//
+// SciRuby is Copyright (c) 2010 - 2014, Ruby Science Foundation
+// NMatrix is Copyright (c) 2012 - 2014, John Woods and the Ruby Science Foundation
+//
+// Please see LICENSE.txt for additional copyright notices.
+//
+// == Contributing
+//
+// By contributing source code to SciRuby, you agree to be bound by
+// our Contributor Agreement:
+//
+// * https://github.com/SciRuby/sciruby/wiki/Contributor-Agreement
+//
+// == nmatrix_fftw.cpp
+//
+// Main file for nmatrix_fftw extension
+//
+
+#include <ruby.h>
+
+#include "nmatrix.h"
+
+#include "data/data.h"
+
+extern "C" {
+  void Init_nmatrix_fftw() 
+  {
+    
+  }
+}
