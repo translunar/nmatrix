43ceb6eb3dc6e502221affcbb612d16ee187eeaa
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-12-16 02:03:38 +0530

    Started adding FFTW tests

--- spec/plugins/fftw/fftw_spec.rb
+++ spec/plugins/fftw/fftw_spec.rb
@@ -27,3 +27,44 @@
 
 require 'spec_helper'
 require "./lib/nmatrix/fftw"
+
+describe NMatrix::FFTW do
+  context ".r2c_one" do
+    it "computes correct FFTW" do
+      n = NMatrix.new([4], [3.10, 1.73, 1.04, 2.83])
+      complex = NMatrix.zeros([3], dtype: :complex128)
+      NMatrix::FFTW.r2c_one(n, complex)
+      # Expected results obtained from running SciPy's fft on the same Array
+      # However, FFTW only computes the first half + 1 element
+
+      exp = NMatrix.new([3], [Complex(8.70, 0), Complex(2.06, 1.1), Complex(-0.42, 0)])
+      expect(complex).to eq(exp)
+    end
+
+    # TODO: Is this test really needed? Moreover, why should the output nmatrix
+    # have shape equal to input nmatrix if FFTW computes only half + 1 elements?
+    it "Checks NMatrix in FFTW.r2c_one and input NMatrix have same value for shape" do
+      n = NMatrix.new([6], [-3.10,
+                            -1.73,
+                             1.0,
+                             2.84,
+                             56.42,
+                             -32.1])
+      complex = NMatrix.zeros([6], dtype: :complex128)
+      fftw = NMatrix::FFTW.r2c_one(n, complex)
+      expect(n.shape).to eq(fftw.shape)
+    end
+
+    it "Checks NMatrix in FFTW.r2c_one and input NMatrix have same value for size" do
+      n = NMatrix.new([6], [-3.10,
+                            -1.73,
+                             1.0,
+                             2.84,
+                             56.42,
+                             -32.1])
+      complex = NMatrix.zeros([6], dtype: :complex128)
+      fftw = FFTW.r2c_one(n, complex)
+      expect(n.size).to eq(fftw.size)
+    end
+  end
+end
