e99b161932e2fb6ea16917fa0aa15165166d4f39
prasun <prasunanand.bitsp@gmail.com>
2016-07-26 00:46:50 +0530

    jruby: correct NMatrix#solve

--- lib/nmatrix/jruby/decomposition.rb
+++ lib/nmatrix/jruby/decomposition.rb
@@ -1,18 +1,29 @@
 class NMatrix
+
+  # discussion in https://github.com/SciRuby/nmatrix/issues/374
   
-  def matrix_solve b
-    if b.shape[1] > 1
+  def matrix_solve rhs
+    if rhs.shape[1] > 1
       nmatrix = NMatrix.new :copy
-      nmatrix.shape = b.shape
+      nmatrix.shape = rhs.shape
       result = []
-      (0...b.shape[1]).each do |i|
-        result.concat(self.solve(b.col(i)).s.toArray.to_a)
+      res = []
+      (0...rhs.shape[1]).each do |i|
+        res << self.solve(rhs.col(i)).s.toArray.to_a
+      end
+      index = 0
+      (0...rhs.shape[0]).each do |i|
+        (0...rhs.shape[1]).each do |j|
+          result[index] = res[j][i]
+          index+=1
+        end
       end
       nmatrix.s = ArrayRealVector.new result.to_java :double
-      nmatrix.twoDMat =  MatrixUtils.createRealMatrix get_twoDArray(b.shape, result)
+      nmatrix.twoDMat =  MatrixUtils.createRealMatrix get_twoDArray(rhs.shape, result)
+      
       return nmatrix
     else
-      return self.solve b
+      return self.solve rhs
     end
   end
 
