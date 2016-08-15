a3144c6d00724308778b364ed882e24287b6b07b
prasun <prasunanand.bitsp@gmail.com>
2016-08-16 04:03:56 +0530

    jruby: correct indentation

--- ext/nmatrix_java/nmatrix/util/WrapperType.java
+++ ext/nmatrix_java/nmatrix/util/WrapperType.java
@@ -3,33 +3,33 @@
 import java.util.*;
 
 public class WrapperType
-{	
-		// How-to-use?
-    // public static void main(String[] args)        
-    // {		Object o = 1;
-    //     System.out.println(isWrapperType(String.class));
-    //     System.out.println(isWrapperType(o.getClass()));
-    // }
+{
+  // How-to-use?
+  // public static void main(String[] args)
+  // {        Object o = 1;
+  //     System.out.println(isWrapperType(String.class));
+  //     System.out.println(isWrapperType(o.getClass()));
+  // }
 
-    private static final Set<Class<?>> WRAPPER_TYPES = getWrapperTypes();
+  private static final Set<Class<?>> WRAPPER_TYPES = getWrapperTypes();
 
-    public static boolean isWrapperType(Class<?> clazz)
-    {
-        return WRAPPER_TYPES.contains(clazz);
-    }
+  public static boolean isWrapperType(Class<?> clazz)
+  {
+    return WRAPPER_TYPES.contains(clazz);
+  }
 
-    private static Set<Class<?>> getWrapperTypes()
-    {
-        Set<Class<?>> ret = new HashSet<Class<?>>();
-        ret.add(Boolean.class);
-        ret.add(Character.class);
-        ret.add(Byte.class);
-        ret.add(Short.class);
-        ret.add(Integer.class);
-        ret.add(Long.class);
-        ret.add(Float.class);
-        ret.add(Double.class);
-        ret.add(Void.class);
-        return ret;
-    }
+  private static Set<Class<?>> getWrapperTypes()
+  {
+    Set<Class<?>> ret = new HashSet<Class<?>>();
+    ret.add(Boolean.class);
+    ret.add(Character.class);
+    ret.add(Byte.class);
+    ret.add(Short.class);
+    ret.add(Integer.class);
+    ret.add(Long.class);
+    ret.add(Float.class);
+    ret.add(Double.class);
+    ret.add(Void.class);
+    return ret;
+  }
 }
