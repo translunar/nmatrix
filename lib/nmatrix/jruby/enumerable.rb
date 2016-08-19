de63a3be910f32fa5b38ca07f04a72f7d8cd8bc0
prasun <prasunanand.bitsp@gmail.com>
2016-08-19 05:50:42 +0530

    jruby: add Enumerable#sum supported by backports gem for MRI

---
+++ lib/nmatrix/jruby/enumerable.rb
@@ -0,0 +1,13 @@
+# Source: https://github.com/marcandre/backports/blob/master/lib/backports/rails/enumerable.rb
+module Enumerable
+  # Standard in rails... See official documentation[http://api.rubyonrails.org/classes/Enumerable.html]
+  # Modified from rails 2.3 to not rely on size
+  def sum(identity = 0, &block)
+    if block_given?
+      map(&block).sum(identity)
+    else
+      inject { |sum, element| sum + element } || identity
+    end
+  end unless method_defined? :sum
+
+end
