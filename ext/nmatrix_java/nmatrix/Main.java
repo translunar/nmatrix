3ad7e0e31e4d14011869cf4700e08d17132d83b0
prasun <prasunanand.bitsp@gmail.com>
2016-10-14 03:33:23 +0530

    jruby: == operator implemented that checks whether two nmatrices are equal

--- ext/nmatrix_java/nmatrix/Main.java
+++ ext/nmatrix_java/nmatrix/Main.java
@@ -6,24 +6,6 @@ import org.apache.commons.math3.linear.ArrayRealVector;
 public class Main{
 
 	public static void main(String[] args){
-		// System.out.println("Hello jnmatrix");
-		JNMatrix a = new JNMatrix(2, new double[]{2,3,4,5}, "FLOAT32", "DENSE_STORE");
-		JNMatrix b = new JNMatrix(2, new double[]{2.2,3,4,5}, "FLOAT32", "DENSE_STORE");
-		JNMatrix c = a.add(b);
-		// float[] abc = new float[4];
-		double[] abc = c.realArray.toArray();
-
-		System.out.println(abc);
-		for(int i=0; i < abc.length; i++){
-  		System.out.println(abc[i]);
-  	}
+		System.out.println("Hello jnmatrix");
 	}
-
-	// public static float[] castDoubleToFloat(double[] elements){
- //  	float[] newElements = new float[elements.length];
- //  	for(int i=0; i < elements.length; i++){
- //  		newElements[i] = (float)elements[i];
- //  	}
- //  	return newElements;
- //  }
 }	
